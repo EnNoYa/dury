dd58e020ab7a5a31ddbb4527a34b7e96f2c2cb53
schyun9212 <schyun9212@gmail.com>
2021-06-04 00:18:12 +0900

    use cookie to prevent captcha

--- dury/crawler/base.py
+++ dury/crawler/base.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
-
+import json
 import time
+import os
 
 
 class SeleniumCrawler:
     def __init__(self, cfg) -> None:
+        self.cookie_file = cfg.SELENIUM.COOKIE_FILE
         self.safe_delay = cfg.SELENIUM.SAFE_DELAY
         self.driver = self.launch(cfg.SELENIUM.CHROMEDRIVER_PATH)
-    
+
     def launch(self, driver_path) -> webdriver:
         options = webdriver.ChromeOptions()
         # options.add_argument("--headless")
@@ -19,3 +21,20 @@ class SeleniumCrawler:
 
     def delay(self) -> None:
         time.sleep(self.safe_delay)
+
+    def save_cookies(self):
+        cookies = self.driver.get_cookies()
+        with open(self.cookie_file, "w") as f:
+            json.dump(cookies, f, indent=4)
+
+    def load_cookies(self, domain):
+        self.driver.get(domain)
+
+        if not os.path.exists(self.cookie_file):
+            return -1
+
+        with open(self.cookie_file, "r") as f:
+                cookies = json.load(f)
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
+        return 0
