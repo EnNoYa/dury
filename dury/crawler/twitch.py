3dda978660bd9278b68168087e0164b62bf0cc11
schyun9212 <schyun9212@gmail.com>
2021-07-10 18:26:40 +0900

    add video api and urls for video download

--- dury/crawler/twitch.py
+++ dury/crawler/twitch.py
@@ -1,10 +1,14 @@
+from os import access
 import requests
 
-from typing import List, Union
+from typing import List, Optional, Union
 
 
 class TwitchClient():
-    BASE_URL = "https://api.twitch.tv/helix"
+    API_BASE_URL = "https://api.twitch.tv/helix"
+    OAUTH_URL = "https://id.twitch.tv/oauth2/token"
+    ACCESS_TOKEN_URL = "https://gql.twitch.tv/gql"
+    VIDEO_SOURCE_URL = "https://usher.ttvnw.net/vod/%s.m3u8?nauthsig=%s&nauth=%s&allow_source=true&player=twitchweb&allow_spectre=true&allow_audio_only=true"
 
     def __init__(self, client_id: str, client_secret: str) -> None:
         self.client_id = client_id
@@ -17,7 +21,7 @@ class TwitchClient():
 
     def get_oauth(self):
         res = requests.post(
-            "https://id.twitch.tv/oauth2/token"
+            self.OAUTH_URL
             + "?client_id=" + self.client_id
             + "&client_secret=" + self.client_secret
             + "&grant_type=client_credentials"
@@ -38,9 +42,27 @@ class TwitchClient():
         channel_information = self._create_request("channels", query)["data"]
         return channel_information
 
+    def get_videos(
+        self,
+        user_id: str, *,
+        after: Optional[str] = None,
+        before: Optional[str] = None,
+        first: Optional[int] = 20
+    ):
+        query = f"user_id={user_id}&first={first}"
+        if after is not None:
+            query += "&after={after}"
+        if before is not None:
+            query += "&before={before}"
+
+        res = self._create_request("videos", query)
+        videos = res["data"]
+        pagination = res["pagination"]
+        return videos, pagination
+
     def _create_request(self, path: str, query: str):
         res = requests.get(
-            f"{self.BASE_URL}/{path}?{query}",
+            f"{self.API_BASE_URL}/{path}?{query}",
             headers=self.headers
         )
         return res.json()
@@ -63,6 +85,8 @@ if __name__ == "__main__":
     client = TwitchClient(client_id, client_secret)
 
     users = client.get_users(["ddahyoni", "pjs9073"])
-    channel_information = client.get_channel_information([user["id"] for user in users])
+    user_ids = [user["id"] for user in users]
+    channel_information = client.get_channel_information(user_ids)
 
+    videos, pagination = client.get_videos(user_ids[0], first=20)
     print("Done")
