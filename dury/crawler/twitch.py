c1946bae28b06340501c012c9b52143d9d78143f
schyun9212 <schyun9212@gmail.com>
2021-07-11 02:13:26 +0900

    add request video playlist

--- dury/crawler/twitch.py
+++ dury/crawler/twitch.py
@@ -5,10 +5,10 @@ from typing import List, Optional, Union
 
 
 class TwitchClient():
-    API_BASE_URL = "https://api.twitch.tv/helix"
+    PUBLIC_API_URL = "https://api.twitch.tv/helix"
     OAUTH_URL = "https://id.twitch.tv/oauth2/token"
-    ACCESS_TOKEN_URL = "https://gql.twitch.tv/gql"
-    VIDEO_SOURCE_URL = "https://usher.ttvnw.net/vod/%s.m3u8?nauthsig=%s&nauth=%s&allow_source=true&player=twitchweb&allow_spectre=true&allow_audio_only=true"
+    PLAYLISTS_URL = "https://usher.ttvnw.net/vod/{}"
+    PRIVATE_API_URL = "https://gql.twitch.tv/gql"
 
     def __init__(self, client_id: str, client_secret: str) -> None:
         self.client_id = client_id
@@ -20,12 +20,11 @@ class TwitchClient():
         }
 
     def get_oauth(self):
-        res = requests.post(
-            self.OAUTH_URL
-            + "?client_id=" + self.client_id
-            + "&client_secret=" + self.client_secret
-            + "&grant_type=client_credentials"
-        )
+        res = requests.post(self.OAUTH_URL, params={
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "grant_type": "client_credentials"
+        })
         oauth = res.json()
         return f"{oauth['token_type'].capitalize()} {oauth['access_token']}"
 
@@ -60,9 +59,53 @@ class TwitchClient():
         pagination = res["pagination"]
         return videos, pagination
 
+    def download_video(self, video_id: str, bitrate: Optional[str] = "720p60"):
+        assert bitrate in [
+            '160p30', '360p30', '480p30', '720p30',
+            '720p60', 'audio_only', 'chunked'
+        ], "Invalid bitrate"
+
+        access_token = self._get_access_token(video_id)["videoPlaybackAccessToken"]
+
+        playlists_url = self.PLAYLISTS_URL.format(video_id)
+        res = requests.get(playlists_url, params={
+            "nauthsig": access_token["signature"],
+            "nauth": access_token["value"],
+            "allow_source": "true",
+            "player": "twitchweb",
+            "allow_spectre": "true",
+            "allow_audio_only": "true"
+        })
+        playlists = res.text.split("\n")
+        playlist_url = list(filter(lambda x: x[:5] == "https" and bitrate in x, playlists))[0]
+
+        return res
+
+    def _get_access_token(self, video_id: str):
+        query = """
+            {{
+                videoPlaybackAccessToken(
+                    id: {video_id},
+                    params: {{
+                        platform: "web",
+                        playerBackend: "mediaplayer",
+                        playerType: "site"
+                    }}
+                ) {{
+                    signature
+                    value
+                }}
+            }}
+        """
+        query = query.format(video_id=video_id)
+        headers = { "Client-ID": "kimne78kx3ncx6brgo4mv6wki5h1ko" }
+        res = requests.post(self.PRIVATE_API_URL, json={"query": query}, headers=headers)
+        access_token = res.json()["data"]
+        return access_token
+
     def _create_request(self, path: str, query: str):
         res = requests.get(
-            f"{self.API_BASE_URL}/{path}?{query}",
+            f"{self.PUBLIC_API_URL}/{path}?{query}",
             headers=self.headers
         )
         return res.json()
@@ -89,4 +132,7 @@ if __name__ == "__main__":
     channel_information = client.get_channel_information(user_ids)
 
     videos, pagination = client.get_videos(user_ids[0], first=20)
+    video_id = "1081430996"
+
+    test = client.download_video(video_id)
     print("Done")
