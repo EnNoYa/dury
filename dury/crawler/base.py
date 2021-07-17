fe768cc2ce777bacb3c046e1050dea14cc92ab57
schyun9212 <schyun9212@gmail.com>
2021-07-17 17:24:16 +0900

    not launch on init

--- dury/crawler/base.py
+++ dury/crawler/base.py
@@ -4,9 +4,9 @@ import os
 from typing import Optional, Any
 
 from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 class SeleniumCrawler:
     def __init__(
@@ -21,41 +21,34 @@ class SeleniumCrawler:
         self.output_dir = output_dir
         self.cookie_file = cookie_file
         self.safe_delay = safe_delay
-        self.driver = self._launch(
-            driver_path,
-            headless=headless,
-            implicitly_wait=implicitly_wait
-        )
+        self.driver_path = driver_path
+        self.headless = headless
+        self.implicitly_wait = implicitly_wait
 
-    def _launch(
-        self,
-        driver_path: str, *,
-        headless: Optional[bool] = False,
-        implicitly_wait: Optional[float] = 10.0
-    ) -> webdriver.Chrome:
+    def _launch(self) -> Chrome:
         options = webdriver.ChromeOptions()
-        if headless:
+        if self.headless:
             options.add_argument("--headless")
         options.add_argument('--no-sandbox')
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--remote-debugging-port=9222")
-        driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
-        driver.implicitly_wait(implicitly_wait)
+        driver = Chrome(executable_path=self.driver_path, chrome_options=options)
+        driver.implicitly_wait(self.implicitly_wait)
         return driver
 
     def _delay(self) -> None:
         time.sleep(self.safe_delay)
 
-    def _explicitly_wait(self, timeout: float, condition: Any):
-        return WebDriverWait(self.driver, timeout).until(condition)
+    def _explicitly_wait(self, driver: Chrome, timeout: float, condition: Any):
+        return WebDriverWait(driver, timeout).until(condition)
 
-    def _save_cookies(self):
-        cookies = self.driver.get_cookies()
+    def _save_cookies(self, driver: Chrome):
+        cookies = driver.get_cookies()
         with open(self.cookie_file, "w") as f:
             json.dump(cookies, f, indent=4)
 
-    def _load_cookies(self, domain: str):
-        self.driver.get(domain)
+    def _load_cookies(self, driver: Chrome, domain: str):
+        driver.get(domain)
 
         if not os.path.exists(self.cookie_file):
             return -1
